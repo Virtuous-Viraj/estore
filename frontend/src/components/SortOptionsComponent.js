aee4f117b71bd92970a826ddbff54d623527eae3
Virtuous-Viraj <virajnerlekar1729@gmail.com>
2023-03-30 23:17:41 +0530

    some changes in accordance with backend

--- frontend/src/components/SortOptionsComponent.js
+++ frontend/src/components/SortOptionsComponent.js
@@ -4,9 +4,9 @@ const SortOptionsComponent = () => {
   return (
     <Form.Select aria-label="Default select example">
       <option value="">SORT BY</option>
-      <option value="price-1">Price: Low To High</option>
-      <option value="price-1">Price: High To Low</option>
-      <option value="rating-1">Customer Rating</option>
+      <option value="price_1">Price: Low To High</option>
+      <option value="price_-1">Price: High To Low</option>
+      <option value="rating_-1">Customer Rating</option>
       <option value="name_1">Name A-Z</option>
       <option value="name_-1">Name Z-A</option>
     </Form.Select>
